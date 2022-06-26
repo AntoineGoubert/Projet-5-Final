6859d5c1d74629943f3577f8033a601747e3ce07
AntoineGoubert <105302550+AntoineGoubert@users.noreply.github.com>
2022-06-26 11:42:22 +0200

    Requirement file

--- appUSE.py
+++ appUSE.py
@@ -1,14 +1,36 @@
 from flask import Flask, render_template, request
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from mlinsights.mlmodel import PredictableTSNE
 import tensorflow_hub as hub
 import pickle
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+from nltk.tokenize import sent_tokenize, word_tokenize
 
 model = pickle.load(open('USE.pkl', 'rb'))
 embedmodel = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
 tsne=pickle.load(open("tsne.pkl",'rb'))
 app = Flask(__name__)
 
+def tokenizer_fct(sentence):
+    sentence_clean = sentence.replace('-', ' ').replace('+', ' ').replace('/', ' ').replace('#', ' ').replace('<', ' ').replace('>', ' ')
+    word_tokens = word_tokenize(sentence_clean)
+    return word_tokens
+
+def lower_start_fct(list_words):
+    lw = [w.lower() for w in list_words if (not w.startswith("@"))
+                                       and (not w.startswith("http"))]
+    return lw
+
+def transform_dl_fct(desc_text) :
+    word_tokens = tokenizer_fct(desc_text)
+    lw = lower_start_fct(word_tokens)
+    transf_desc_text = ' '.join(lw)
+    return transf_desc_text
 
 @app.route('/', methods=['POST', 'GET'])
 def new():
@@ -21,8 +43,8 @@ def predict():
     data2 = str(request.form['b'])
     alldata = data1 + ' ' + data2
 
-    features=embedmodel.predict(alldata)
-    pred = model.predict([features])
+    features=tsne.transform(embedmodel([transform_dl_fct(alldata)]))
+    pred = model.predict(features)
 
     def statement():
         return('The most appropriate tag for your entry is ' + pred)
