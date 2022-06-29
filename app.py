777102375ead834caa6dba3788434a098041572a
AntoineGoubert <105302550+AntoineGoubert@users.noreply.github.com>
2022-06-29 16:57:59 +0200

    App Update

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import tensorflow_hub as hub
 import mlinsights.mlmodel
 import pickle
 import streamlit as st
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 model = pickle.load(open('USE.pkl', 'rb'))
 embedmodel = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
