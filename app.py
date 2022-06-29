99e431f48d9937e79edc817d318e91dba896011d
AntoineGoubert <105302550+AntoineGoubert@users.noreply.github.com>
2022-06-29 14:13:10 +0200

    App Update

--- app.py
+++ app.py
@@ -1,37 +1,17 @@
 from flask import Flask, render_template, request
 import tensorflow_hub as hub
 import pickle
-import os
-from pml import app
-
-port = int(os.getenv('PORT'))
+import streamlit as st
 
 model = pickle.load(open('USE.pkl', 'rb'))
 embedmodel = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
 tsne=pickle.load(open("tsne.pkl",'rb'))
 
-app = Flask(__name__)
+data1 = str(request.form['a'])
+data2 = str(request.form['b'])
+alldata = data1 + ' ' + data2
 
+features=tsne.transform(embedmodel([alldata]))
+pred = model.predict(features)
 
-@app.route('/', methods=['POST', 'GET'])
-def new():
-    return render_template('new.html')
-
-
-@app.route('/predict', methods=['POST', 'GET'])
-def predict():
-    data1 = str(request.form['a'])
-    data2 = str(request.form['b'])
-    alldata = data1 + ' ' + data2
-
-    features=tsne.transform(embedmodel([alldata]))
-    pred = model.predict(features)
-
-    def statement():
-        return('The most appropriate tag for your entry is ' + pred)
-
-    return render_template('new.html', statement=statement())
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port)
+st.write(pred)
