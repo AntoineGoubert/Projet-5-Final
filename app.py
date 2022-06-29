ef476abea75f34410a45d4e023e3e0adfe45806e
AntoineGoubert <105302550+AntoineGoubert@users.noreply.github.com>
2022-06-29 15:47:49 +0200

    App Update

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ model = pickle.load(open('USE.pkl', 'rb'))
 embedmodel = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
 tsne=pickle.load(open("tsne.pkl",'rb'))
 
-alldata = st.text_input("Texte")
+data1=st.text_input("Titre")
+data2=st.text_input("Texte")
+alldata = data1 + ' ' + data2
 
 features=tsne.transform(embedmodel([alldata]))
 pred = model.predict(features)
